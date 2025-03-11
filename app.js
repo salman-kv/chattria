0b46f76c45e8d0fa326c8889c0b2945f5f9d19c1
Sh4b33r <borntough0101@gmail.com>
2025-03-12 01:39:18 +0530

    chatting dummy layout done, by clicking the user

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://127.0.0.1:27017/chattria').then(() => console.log('mongo connected'));
 
+const express = require('express');
 const app = require('express')();
 const userRouter = require('../chattria/routes/user_routes');
+
+app.use(express.static('public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user_model');
+const session = require('express-session');
 
 const registerLoad = async (req, res) => {
     res.render('register');
@@ -26,57 +27,57 @@ const register = async (req, res) => {
     }
 }
 
-const loginLoad = (req,res)=>{
+const loginLoad = (req, res) => {
     try {
         res.render('login');
     } catch (error) {
         console.log(error.message);
     }
 }
-const login = async (req,res)=>{
+const login = async (req, res) => {
     try {
         const email = req.body.email;
         const password = req.body.password;
 
-        const userData = await User.findOne({email:email});
-        
-        if(userData){
-            console.log(userData);
-            const passwordMatch = await bcrypt.compare(password,userData.password);
-            if(passwordMatch){
+        const userData = await User.findOne({ email: email });
+
+        if (userData) {
+            const passwordMatch = await bcrypt.compare(password, userData.password);
+            if (passwordMatch) {
                 console.log('true');
                 req.session.user = userData;
                 res.redirect('/user/dashboard');
-            }else{
+            } else {
                 console.log('false');
-                res.render('login',{message:'Incorrect email or password'});
+                res.render('login', { message: 'Incorrect email or password' });
             }
 
 
-        }else{
-            res.render('login',{message:'Incorrect email or password'});
+        } else {
+            res.render('login', { message: 'Incorrect email or password' });
         }
-        
+
     } catch (error) {
         console.log(error.message);
     }
 }
 
-const logout = (req,res)=>{
+const logout = (req, res) => {
     try {
         req.session.destroy();
         res.redirect('/user/login');
-        
+
     } catch (error) {
         console.log(error.message);
     }
 }
 
-const dashboardLoad = (req,res)=>{
+const dashboardLoad = async (req, res) => {
     try {
-        console.log(req.session.user);
+        var users = await User.find({_id : {$nin:[req.session.user._id] }});
+        console.log(users);
         
-       res.render('dashboard',{user:req.session.user}); 
+        res.render('dashboard', { user: req.session.user, users : users });
     } catch (error) {
         console.log(error.message);
     }
