5e9796798f2880601f8c52c16325e7b6f3dfc8fe
Sh4b33r <borntough0101@gmail.com>
2025-03-13 02:30:12 +0530

    created socket io on server and connected on front end

--- app.js
+++ app.js
@@ -18,6 +18,25 @@ app.use('/user', userRouter);
 
 const http = require('http').Server(app);
 
+const User = require('../chattria/models/user_model');
+const io = require('socket.io')(http);
+var usp = io.of('/user-namespace');
+
+usp.on('connection', async (socket) => {
+
+    console.log('connected socket io');
+    var token = socket.handshake.auth.token;
+
+    await User.findByIdAndUpdate({_id: token },{$set:{ is_online : true}});
+    
+
+    socket.on('disconnect',async function(){
+        console.log('disconnected socket io');
+        await User.findByIdAndUpdate({_id: token },{$set:{ is_online : false}});
+    })
+
+})
+
 http.listen(3000, function () {
     console.log('server is running');
 
